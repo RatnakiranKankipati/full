bdd4bab44083d288256d53dbb3165ce4c4b1c41c
RatnakiranKankipati <114407252+RatnakiranKankipati@users.noreply.github.com>
2023-08-15 23:57:44 -0800

    Delete server directory

--- server/db/connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require("mongoose")
-
-mongoose.connect("mongodb://localhost:27017/project")
-.then(()=>{
-    console.log("connection is estibilished");
-})
-.catch((err)=>{
-    console.log(`error is: ${err} `);
-})
